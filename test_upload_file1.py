15758c56108e5ce08b220e1f2d56f288dd4ddca4
Artjoms Dobiss <archie.dobiss@gmail.com>
2022-04-20 00:08:13 +0100

    Update file

--- test_upload_file1.py
+++ test_upload_file1.py
@@ -1,8 +1,22 @@
 import requests
+import os
 
 url = 'http://127.0.0.1:8000/files/'
+
+# File upload and resampling
 file = {'inFile': open(r"C:\Users\AD\Desktop\You Tell Me.mp3", 'rb')}
 response = requests.post(url=url, files=file)
-json_data = response.json()
 file = {'inFile': (open(r"C:\Users\AD\Desktop\You Tell Me.mp3", 'rb')).close()}
+json_data = response.json()
+
 print(json_data)
+
+# Resampled file download
+download_file_path = json_data['file path']
+query_params = {'file_path': download_file_path}
+download_response = requests.get(url=url, params=query_params)
+open(download_file_path, 'wb').write(download_response.content)
+
+
+# upload to temp or project folder
+# download to temp/ project/ user-specified foler
--- upload_file1.py
+++ upload_file1.py
@@ -6,33 +6,40 @@ import ffmpeg
 import os
 from pathlib import Path
 
+# Utils
+working_dir = Path().resolve()
 app = FastAPI()
-output_file_path = r"C:\Users\AD\Desktop\You Tell Me-resampled.mp3"
 
+# Upload and resample audio file
 @app.post("/files/")
-def file_upload(inFile: UploadFile):
+async def file_upload(inFile: UploadFile):
     try:
-       suffix = Path(inFile.filename).suffix
-       with NamedTemporaryFile(delete=False, suffix=suffix) as tmp:
+       #suffix = Path(inFile.filename).suffix
+       with NamedTemporaryFile(delete=False) as tmp:
             shutil.copyfileobj(inFile.file, tmp)
             tmp_path = Path(tmp.name)
-            #global output_file_path
-            #output_file_path = tmp_path + '-resampled.mp3'
+                        
+            # Create filename for resampled file
+            base_filename = os.path.splitext(inFile.filename)[0]
+            temp_filename = os.path.basename(tmp_path)
+            
+            # Resample file
             stream = ffmpeg.input(tmp_path)
-            audio = stream.audio
+            audio = stream.audio                       
+            output_file_path = os.path.join(working_dir, temp_filename + ".mp3") # Downloads to project folder
             stream = ffmpeg.output(audio, output_file_path, **{'ar': '16000','acodec':'mp3'})
             ffmpeg.run(stream, capture_stdout=True, capture_stderr=True)
-          
-        #return {tmp_path, suffix}
+            
+            return {'file name': base_filename, 'file path': output_file_path, 'tmp path': tmp_path}
     #except BaseException as err:
         #return {f"Unexpected {err}, {type(err)=}"}
     
     finally:
-        #inFile.file.close()
+        inFile.filename
         tmp_path.unlink()
-    return {output_file_path}
+#return {output_file_path}
 
 @app.get("/files/", response_class=FileResponse)
-async def main():
-    return output_file_path
+async def main(file_path: str):
+    return file_path
 #os.remove(output_file_path)

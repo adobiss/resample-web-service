ab0452df74a679754e19e8e25c5d0f5620557f79
Artjoms Dobiss <archie.dobiss@gmail.com>
2022-04-20 12:41:15 +0100

    Update test_upload_file1.py

--- test_upload_file1.py
+++ test_upload_file1.py
@@ -1,22 +1,31 @@
 import requests
+import uvicorn
 import os
 
-url = 'http://127.0.0.1:8000/files/'
+url = 'http://127.0.0.1:8000/resample/'
+audio_to_resample_path = r"C:\Users\AD\Desktop\You Tell Me.wav" # add local file path
 
 # File upload and resampling
-file = {'inFile': open(r"C:\Users\AD\Desktop\You Tell Me.mp3", 'rb')}
+file = {'inFile': open(audio_to_resample_path, 'rb')}
 response = requests.post(url=url, files=file)
-file = {'inFile': (open(r"C:\Users\AD\Desktop\You Tell Me.mp3", 'rb')).close()}
+file = {'inFile': (open(audio_to_resample_path, 'rb')).close()}
 json_data = response.json()
 
-print(json_data)
+# Resampled file download to working directory
+download_file_path = json_data['file_path']
+resampled_file_name = json_data['file_name']
 
-# Resampled file download
-download_file_path = json_data['file path']
+working_dir = json_data['working_dir']
+
+client_download_file_path = resampled_file_name + '-resampled.mp3'
+client_download_file_path_full = working_dir + '\\' + client_download_file_path
+
 query_params = {'file_path': download_file_path}
+
 download_response = requests.get(url=url, params=query_params)
-open(download_file_path, 'wb').write(download_response.content)
+open(client_download_file_path, 'wb').write(download_response.content)
 
+# Delete temp file
+os.remove(download_file_path) 
 
-# upload to temp or project folder
-# download to temp/ project/ user-specified foler
+print('Audio resampled successfully: ' + client_download_file_path_full)
